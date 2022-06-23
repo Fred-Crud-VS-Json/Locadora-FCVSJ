093577acc49a173a0bdc2a1817d6a8df1e12d91b
Eduardo Henckemaier <eduhenckemaier@outlook.com>
2022-06-23 16:28:19 -0300

    Implementado o bando de dados funcionario

---
+++ LocadoraFCVSJ.Projeto.BancoDeDados/dbo/Tables/TBFuncionario.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[TBFuncionario] (
+    [Id]           INT          IDENTITY (1, 1) NOT NULL,
+    [Nome]         VARCHAR (50) NOT NULL,
+    [Login]        VARCHAR (50) NOT NULL,
+    [Senha]        VARCHAR (50) NOT NULL,
+    [Salario]      DECIMAL (18) NOT NULL,
+    [DataAdmissao] DATETIME     NOT NULL,
+    [NivelAcesso]  INT          NOT NULL,
+    CONSTRAINT [PK_TBFuncionario] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
