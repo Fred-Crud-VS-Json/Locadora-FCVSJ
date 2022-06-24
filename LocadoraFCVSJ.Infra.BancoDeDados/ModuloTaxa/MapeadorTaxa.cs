818c50fa03ee802af5fbf0590a085a60e39c1833
Alan Fernandes <alanfernandes78@hotmail.com>
2022-06-24 15:18:27 -0300

    Implementado a tabela e Infra do módulo taxa

---
+++ LocadoraFCVSJ.Infra.BancoDeDados/ModuloTaxa/MapeadorTaxa.cs
@@ -0,0 +1,34 @@
+﻿using LocadoraFCVSJ.Dominio.Compartilhado;
+using LocadoraFCVSJ.Dominio.ModuloTaxas;
+using LocadoraFCVSJ.Infra.BancoDeDados.Compartilhado;
+using System.Data.SqlClient;
+
+namespace LocadoraFCVSJ.Infra.BancoDeDados.ModuloTaxa
+{
+    public class MapeadorTaxa : MapeadorBase<Taxa>
+    {
+        public override void ConfigurarParametros(Taxa taxa, SqlCommand comando)
+        {
+            comando.Parameters.AddWithValue("ID", taxa.Id);
+            comando.Parameters.AddWithValue("NOME", taxa.Nome);
+            comando.Parameters.AddWithValue("VALOR", taxa.Valor);
+            comando.Parameters.AddWithValue("TIPOCALCULOTAXA", taxa.TipoCalculoTaxa);
+        }
+
+        public override Taxa ConverterRegistro(SqlDataReader leitorRegistro)
+        {
+            int id = Convert.ToInt32(leitorRegistro["ID"]);
+            string? nome = Convert.ToString(leitorRegistro["NOME"]);
+            decimal valor = Convert.ToDecimal(leitorRegistro["VALOR"]);
+            TipoCalculoTaxa tipoCalculoTaxa = (TipoCalculoTaxa)leitorRegistro["TIPOCALCULOTAXA"];
+
+            return new Taxa
+            {
+                Id = id,
+                Nome = nome,
+                Valor = valor,
+                TipoCalculoTaxa = tipoCalculoTaxa
+            };
+        }
+    }
+}
---
+++ LocadoraFCVSJ.Infra.BancoDeDados/ModuloTaxa/RepositorioTaxa.cs
@@ -0,0 +1,57 @@
+﻿using LocadoraFCVSJ.Dominio.ModuloTaxas;
+using LocadoraFCVSJ.Infra.BancoDeDados.Compartilhado;
+
+namespace LocadoraFCVSJ.Infra.BancoDeDados.ModuloTaxa
+{
+    public class RepositorioTaxa : RepositorioBase<Taxa, TaxaValidador, MapeadorTaxa>
+    {
+        protected override string QueryInserir =>
+            @"INSERT INTO [TBTaxa]
+                (
+                    [NOME],
+                    [VALOR],
+                    [TIPOCALCULOTAXA]
+                )
+                VALUES
+                (
+                    @NOME,
+                    @VALOR,
+                    @TIPOCALCULOTAXA
+                )
+                SELECT SCOPE_IDENTITY()";
+
+        protected override string QueryEditar =>
+             @"UPDATE [TBTaxa]
+	            SET 
+                    [NOME] = @NOME,
+                    [VALOR] = @VALOR,
+                    [TIPOCALCULOTAXA] = @TIPOCALCULOTAXA
+	            WHERE
+		            [ID] = @ID";
+
+        protected override string QueryExcluir =>
+            @"DELETE FROM [TBTaxa] 
+                WHERE 
+                    [ID] = @ID";
+
+        protected override string QuerySelecionarPorId =>
+            @"SELECT 
+	                TAXA.[ID],
+	                TAXA.[NOME],
+	                TAXA.[VALOR],
+	                TAXA.[TIPOCALCULOTAXA]
+                FROM
+	                [TBTaxa] AS TAXA
+                WHERE 
+	                TAXA.[ID] = @ID";
+
+        protected override string QuerySelecionarTodos =>
+            @"SELECT 
+	                TAXA.[ID],
+	                TAXA.[NOME],
+	                TAXA.[VALOR],
+	                TAXA.[TIPOCALCULOTAXA]
+                FROM
+	                [TBTaxa] AS TAXA";
+    }
+}
