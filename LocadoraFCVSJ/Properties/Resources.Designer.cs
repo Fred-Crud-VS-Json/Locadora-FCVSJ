411b7e10789af6e878ab45dbe30cf368c313b241
Alan Fernandes <alanfernandes78@hotmail.com>
2022-06-24 01:39:13 -0300

    Correção em um icone na tela principal

--- LocadoraFCVSJ/Properties/Resources.Designer.cs
+++ LocadoraFCVSJ/Properties/Resources.Designer.cs
@@ -93,16 +93,6 @@ namespace LocadoraFCVSJ.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        internal static System.Drawing.Bitmap close_20px1 {
-            get {
-                object obj = ResourceManager.GetObject("close_20px1", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
         internal static System.Drawing.Bitmap info_30px {
             get {
                 object obj = ResourceManager.GetObject("info_30px", resourceCulture);
