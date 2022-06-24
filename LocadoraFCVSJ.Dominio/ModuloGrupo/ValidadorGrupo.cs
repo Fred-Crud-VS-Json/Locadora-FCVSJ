b9ca71d581ec22bdf2c66c0eff2004531567cc0b
Alan Fernandes <alanfernandes78@hotmail.com>
2022-06-24 11:33:32 -0300

    Refatoração no ValidadorGrupo

--- LocadoraFCVSJ.Dominio/ModuloGrupo/ValidadorGrupo.cs
+++ LocadoraFCVSJ.Dominio/ModuloGrupo/ValidadorGrupo.cs
@@ -8,8 +8,7 @@ namespace LocadoraFCVSJ.Dominio.ModuloGrupo
         public ValidadorGrupo()
         {
             RuleFor(x => x.Nome)
-                .Matches(new Regex(@"^[ a-zA-Z-à-ü]{3,60}$")).WithMessage("Nome informado é inválido.")
-                .NotEmpty().WithMessage("'Nome' deve ser preenchido.");
+                .Matches(new Regex(@"^[ a-zA-Z-à-ü]{3,60}$")).WithMessage("Nome informado é inválido. Lembre-se de preenche-lo corretamente. \n(Min 3 caracteres, Max 60 caracteres) \n(Acentos e espaços são permitidos)\n");
         }
     }
 }
