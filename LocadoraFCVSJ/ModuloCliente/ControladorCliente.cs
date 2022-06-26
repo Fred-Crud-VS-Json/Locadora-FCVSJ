33fe9ad7d3c145fb9d8b5549ac21ce7ab5916d42
Pedro Nunes <pedronunesjc@gmail.com>
2022-06-26 17:11:51 -0300

    Implementando código no registrarClientes

---
+++ LocadoraFCVSJ/ModuloCliente/ControladorCliente.cs
@@ -0,0 +1,53 @@
+﻿using Krypton.Toolkit;
+using LocadoraFCVSJ.Compartilhado;
+using LocadoraFCVSJ.Infra.BancoDeDados.ModuloCliente;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocadoraFCVSJ.ModuloCliente
+{
+    public class ControladorCliente : ControladorBase
+    {
+        private readonly RepositorioClienteEmBancoDeDados _repositorioCliente;
+        private readonly ControleClienteForm controleClienteForm;
+
+        public ControladorCliente(RepositorioClienteEmBancoDeDados repositorioCliente)
+        {
+            _repositorioCliente = repositorioCliente;
+            controleClienteForm = new(this);
+        }
+
+        public override void Inserir()
+        {
+            RegistrarNovoCliente tela = new()
+            {
+                Cliente = new(),
+                SalvarRegistro = _repositorioCliente.Inserir
+            };
+
+            DialogResult resultado = tela.ShowDialog();
+
+            if (resultado == DialogResult.OK);
+               // CarregarClientes();
+        }
+
+        public override void Editar()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Excluir()
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public override KryptonForm ObterTela()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- LocadoraFCVSJ/ModuloCliente/RegistrarNovoCliente.cs
+++ LocadoraFCVSJ/ModuloCliente/RegistrarNovoCliente.cs
@@ -1,17 +1,81 @@
-﻿using Krypton.Toolkit;
+﻿using FluentValidation.Results;
+using Krypton.Toolkit;
+using LocadoraFCVSJ.Compartilhado;
+using LocadoraFCVSJ.Dominio.ModuloCliente;
 
 namespace LocadoraFCVSJ.ModuloCliente
 {
     public partial class RegistrarNovoCliente : KryptonForm
     {
+        private Cliente cliente;
+
         public RegistrarNovoCliente()
         {
             InitializeComponent();
+
+            List<EstadosUF> estadosUF = Enum.GetValues(typeof(EstadosUF)).Cast<EstadosUF>().ToList();
+
+            estadosUF.ForEach(x => CbxUf.Items.Add(x));
         }
+
+        public Cliente Cliente
+        {
+            get { return cliente; }
+
+            set
+            {
+                cliente = value;
+
+                TxbNome.Text = cliente.Nome;
+                MTxbCpf.Text = Convert.ToString(cliente.CPF);
+                MtxbCnpj.Text = Convert.ToString(cliente.CNPJ);
+                TxbCnh.Text = Convert.ToString(cliente.CNH);
+                MtxbTelefone.Text = Convert.ToString(cliente.Telefone);
+                TxbEmail.Text = cliente.Email;
+                TxbCidade.Text = cliente.Cidade;
+                MtxbCep.Text = Convert.ToString(cliente.CEP);
+                TxbNumero.Text = Convert.ToString(cliente.Numero);
+                TxbBairro.Text = cliente.Bairro;
+                CbxUf.SelectedItem = cliente.UF;
+                TxbComplemento.Text = cliente.Complemento;
+                TxbRua.Text = cliente.Rua;
+            }
+        }
+        public Func<Cliente, ValidationResult> SalvarRegistro { get; set; }
 
         private void BtnConcluirRegistro_Click(object sender, EventArgs e)
         {
+            try
+            {
+                TxbNome.Text = cliente.Nome;
+                MTxbCpf.Text = Convert.ToString(cliente.CPF);
+                MtxbCnpj.Text = Convert.ToString(cliente.CNPJ);
+                TxbCnh.Text = Convert.ToString(cliente.CNH);
+                MtxbTelefone.Text = Convert.ToString(cliente.Telefone);
+                TxbEmail.Text = cliente.Email;
+                TxbCidade.Text = cliente.Cidade;
+                MtxbCep.Text = Convert.ToString(cliente.CEP);
+                TxbNumero.Text = Convert.ToString(cliente.Numero);
+                TxbBairro.Text = cliente.Bairro;
+                CbxUf.SelectedItem = cliente.UF;
+                TxbComplemento.Text = cliente.Complemento;
+                TxbRua.Text = cliente.Rua;
 
+
+                ValidationResult resultado = SalvarRegistro(cliente);
+
+                if (resultado.IsValid == false)
+                {
+                    MessageBox.Show(resultado.ToString("\n"), Text, MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+
+                    DialogResult = DialogResult.None;
+                }
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("O campo 'CPF', 'CNH', 'NUMERO', 'CNPJ', 'TELEFONE' E 'CEP' devem conter apenas números.", Text, MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                DialogResult = DialogResult.None;
+            }
         }
     }
 }
