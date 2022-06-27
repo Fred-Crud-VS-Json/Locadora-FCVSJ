d1eff626b00d96d9d410938b7928285dab70b064
Alan Fernandes <alanfernandes78@hotmail.com>
2022-06-27 14:43:10 -0300

    Alteração nos data types da tabela TBFuncionario

--- LocadoraFCVSJ.Projeto.BancoDeDados/dbo/Tables/TBFuncionario.sql
+++ LocadoraFCVSJ.Projeto.BancoDeDados/dbo/Tables/TBFuncionario.sql
@@ -1,9 +1,9 @@
 ﻿CREATE TABLE [dbo].[TBFuncionario] (
     [Id]           INT          IDENTITY (1, 1) NOT NULL,
-    [Nome]         VARCHAR (50) NOT NULL,
-    [Login]        VARCHAR (50) NOT NULL,
-    [Senha]        VARCHAR (50) NOT NULL,
-    [Salario]      DECIMAL (18) NOT NULL,
+    [Nome]         VARCHAR (100) NOT NULL,
+    [Login]        VARCHAR (100) NOT NULL,
+    [Senha]        VARCHAR (100) NOT NULL,
+    [Salario]      DECIMAL (18, 2) NOT NULL,
     [DataAdmissao] DATETIME     NOT NULL,
     [NivelAcesso]  INT          NOT NULL,
     CONSTRAINT [PK_TBFuncionario] PRIMARY KEY CLUSTERED ([Id] ASC)
