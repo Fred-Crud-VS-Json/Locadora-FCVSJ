4589bf16e25a00d09401152d776bfe6d2708c3b0
Alan Fernandes <alanfernandes78@hotmail.com>
2022-06-29 16:23:57 -0300

    Implementado a interface IRepositorioGrupo

---
+++ LocadoraFCVSJ.Infra.BancoDeDados/ModuloGrupo/IRepositorioGrupo.cs
@@ -0,0 +1,10 @@
+﻿using LocadoraFCVSJ.Dominio.ModuloGrupo;
+using LocadoraFCVSJ.Infra.BancoDeDados.Compartilhado;
+
+namespace LocadoraFCVSJ.Infra.BancoDeDados.ModuloGrupo
+{
+    public interface IRepositorioGrupo : IRepositorio<Grupo>
+    {
+        Grupo? SelecionarPropriedade<T>(string parametro, T propriedade);
+    }
+}
