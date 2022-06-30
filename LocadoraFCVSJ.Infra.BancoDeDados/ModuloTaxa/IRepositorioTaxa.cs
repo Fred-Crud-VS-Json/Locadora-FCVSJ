7208d1004c2d6a4010e7b7295578eb6e107f4f29
Alan Fernandes <alanfernandes78@hotmail.com>
2022-06-30 00:05:35 -0300

    Implementado a interface IRepositorioTaxa

---
+++ LocadoraFCVSJ.Infra.BancoDeDados/ModuloTaxa/IRepositorioTaxa.cs
@@ -0,0 +1,10 @@
+﻿using LocadoraFCVSJ.Dominio.ModuloTaxa;
+using LocadoraFCVSJ.Infra.BancoDeDados.Compartilhado;
+
+namespace LocadoraFCVSJ.Infra.BancoDeDados.ModuloTaxa
+{
+    public interface IRepositorioTaxa : IRepositorio<Taxa>
+    {
+        Taxa? SelecionarPropriedade<T>(string query, string parametro, T propriedade);
+    }
+}
