76373ee573d604dd4d8b63f00a77177e086952bf
Pedro Nunes <pedronunesjc@gmail.com>
2022-06-30 14:22:21 -0300

    Implementado IRepositorioCliente

---
+++ LocadoraFCVSJ.Infra.BancoDeDados/ModuloCliente/IRepositorioCliente.cs
@@ -0,0 +1,10 @@
+﻿using LocadoraFCVSJ.Dominio.ModuloCliente;
+using LocadoraFCVSJ.Infra.BancoDeDados.Compartilhado;
+
+namespace LocadoraFCVSJ.Infra.BancoDeDados.ModuloCliente
+{
+    public interface IRepositorioCliente : IRepositorio<Cliente>
+    {
+        Cliente? SelecionarPropriedade<T>(string parametro, T propriedade);
+    }
+}
