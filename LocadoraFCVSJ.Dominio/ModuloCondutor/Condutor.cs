9e430a397df55a96c4a4332a19d6bc6b5e40977e
Pedro Nunes <pedronunesjc@gmail.com>
2022-06-30 16:49:11 -0300

    Implementado classe Condutor

---
+++ LocadoraFCVSJ.Dominio/ModuloCondutor/Condutor.cs
@@ -0,0 +1,84 @@
+﻿using LocadoraFCVSJ.Compartilhado;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocadoraFCVSJ.Dominio.ModuloCondutor
+{
+    public class Condutor
+    {
+          public Condutor(string nome, string cPF, string? cNPJ, string cNH, DateTime dataVencimento, string telefone, string email, string cidade, string cEP, string numero, string bairro, UF? uF, string? complemento, string rua)
+        {
+            Nome = nome;
+            CPF = cPF;
+            CNPJ = cNPJ;
+            this.CNH = cNH;
+            DataVencimento = dataVencimento;
+            Telefone = telefone;
+            Email = email;
+            Cidade = cidade;
+            CEP = cEP;
+            Numero = numero;
+            Bairro = bairro;
+            UF = uF;
+            Complemento = complemento;
+            Rua = rua;
+        }
+
+        public string Nome { get; set; }
+        public string CPF { get; set; }
+        public string? CNPJ { get; set; }
+        public string CNH { get; set; }
+        public DateTime DataVencimento { get; set; }
+        public string Telefone { get; set; }
+        public string Email { get; set; }
+        public string Cidade { get; set; }
+        public string CEP { get; set; }
+        public string Numero { get; set; }
+        public string Bairro { get; set; }
+        public UF? UF { get; set; }
+        public string? Complemento { get; set; }
+        public string Rua { get; set; }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is Condutor condutor &&
+                   Nome == condutor.Nome &&
+                   CPF == condutor.CPF &&
+                   CNPJ == condutor.CNPJ &&
+                   CNH == condutor.CNH &&
+                   DataVencimento == condutor.DataVencimento &&
+                   Telefone == condutor.Telefone &&
+                   Email == condutor.Email &&
+                   Cidade == condutor.Cidade &&
+                   CEP == condutor.CEP &&
+                   Numero == condutor.Numero &&
+                   Bairro == condutor.Bairro &&
+                   UF == condutor.UF &&
+                   Complemento == condutor.Complemento &&
+                   Rua == condutor.Rua;
+        }
+
+        public override int GetHashCode()
+        {
+            HashCode hash = new HashCode();
+            hash.Add(Nome);
+            hash.Add(CPF);
+            hash.Add(CNPJ);
+            hash.Add(CNH);
+            hash.Add(DataVencimento);
+            hash.Add(Telefone);
+            hash.Add(Email);
+            hash.Add(Cidade);
+            hash.Add(CEP);
+            hash.Add(Numero);
+            hash.Add(Bairro);
+            hash.Add(UF);
+            hash.Add(Complemento);
+            hash.Add(Rua);
+            return hash.ToHashCode();
+        }
+    }
+}
---
+++ LocadoraFCVSJ.Dominio/ModuloCondutor/ValidadorCondutor.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocadoraFCVSJ.Dominio.ModuloCondutor
+{
+    public class ValidadorCondutor
+    {
+    }
+}
