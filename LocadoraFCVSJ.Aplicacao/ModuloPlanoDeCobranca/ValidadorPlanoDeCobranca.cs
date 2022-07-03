f9b8fda08424d8aed4642e1feafe67c6cac42fb0
Alan Fernandes <alanfernandes78@hotmail.com>
2022-07-03 15:18:28 -0300

    Implementado o validador do PlanoDeCobrança

---
+++ LocadoraFCVSJ.Aplicacao/ModuloPlanoDeCobranca/ValidadorPlanoDeCobranca.cs
@@ -0,0 +1,28 @@
+﻿using FluentValidation;
+
+namespace LocadoraFCVSJ.Aplicacao.ModuloPlanoDeCobranca
+{
+    public class ValidadorPlanoDeCobranca : AbstractValidator<PlanoDeCobranca>
+    {
+        public ValidadorPlanoDeCobranca()
+        {
+            RuleFor(x => x.PlanoDiarioValorDiario)
+                .GreaterThan(0);
+
+            RuleFor(x => x.PlanoDiarioValorKm)
+                .GreaterThan(0);
+
+            RuleFor(x => x.PlanoLivreValorDiario)
+                .GreaterThan(0);
+
+            RuleFor(x => x.PlanoControladoValorDiario)
+                .GreaterThan(0);
+
+            RuleFor(x => x.PlanoControladoValorKm)
+                 .GreaterThan(0);
+
+            RuleFor(x => x.PlanoControladoLimiteKm)
+                .GreaterThan(0);
+        }
+    }
+}
