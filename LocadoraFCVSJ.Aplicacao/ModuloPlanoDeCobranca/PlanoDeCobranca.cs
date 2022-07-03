d716fd790f0c2aafa3deb65388cde836da1584d6
Alan Fernandes <alanfernandes78@hotmail.com>
2022-07-03 15:09:49 -0300

    Implementado o objeto PlanoDeCobranca

---
+++ LocadoraFCVSJ.Aplicacao/ModuloPlanoDeCobranca/PlanoDeCobranca.cs
@@ -0,0 +1,32 @@
+﻿using LocadoraFCVSJ.Dominio.ModuloGrupo;
+
+namespace LocadoraFCVSJ.Aplicacao.ModuloPlanoDeCobranca
+{
+    public class PlanoDeCobranca
+    {
+        public Grupo Grupo { get; set; }
+        public decimal PlanoDiarioValorDiario { get; set; }
+        public decimal PlanoDiarioValorKm { get; set; }
+        public decimal PlanoLivreValorDiario { get; set; }
+        public decimal PlanoControladoValorDiario { get; set; }
+        public decimal PlanoControladoValorKm { get; set; }
+        public int PlanoControladoLimiteKm { get; set; }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is PlanoDeCobranca cobranca &&
+                   EqualityComparer<Grupo>.Default.Equals(Grupo, cobranca.Grupo) &&
+                   PlanoDiarioValorDiario == cobranca.PlanoDiarioValorDiario &&
+                   PlanoDiarioValorKm == cobranca.PlanoDiarioValorKm &&
+                   PlanoLivreValorDiario == cobranca.PlanoLivreValorDiario &&
+                   PlanoControladoValorDiario == cobranca.PlanoControladoValorDiario &&
+                   PlanoControladoValorKm == cobranca.PlanoControladoValorKm &&
+                   PlanoControladoLimiteKm == cobranca.PlanoControladoLimiteKm;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Grupo, PlanoDiarioValorDiario, PlanoDiarioValorKm, PlanoLivreValorDiario, PlanoControladoValorDiario, PlanoControladoValorKm, PlanoControladoLimiteKm);
+        }
+    }
+}
