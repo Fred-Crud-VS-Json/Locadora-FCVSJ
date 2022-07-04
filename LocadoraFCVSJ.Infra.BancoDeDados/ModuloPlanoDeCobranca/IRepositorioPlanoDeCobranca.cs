3974d4c230c579c5e21a8fdabf6ab4184cce77e9
Alan Fernandes <alanfernandes78@hotmail.com>
2022-07-04 14:23:54 -0300

    Implementado a interface IRepositorioPlanoDeCobranca

---
+++ LocadoraFCVSJ.Infra.BancoDeDados/ModuloPlanoDeCobranca/IRepositorioPlanoDeCobranca.cs
@@ -0,0 +1,9 @@
+﻿using LocadoraFCVSJ.Dominio.ModuloPlanoDeCobranca;
+
+namespace LocadoraFCVSJ.Infra.BancoDeDados.ModuloPlanoDeCobranca
+{
+    public interface IRepositorioPlanoDeCobranca
+    {
+        PlanoDeCobranca? SelecionarPropriedade<T>(string query, string parametro, T propriedade);
+    }
+}
