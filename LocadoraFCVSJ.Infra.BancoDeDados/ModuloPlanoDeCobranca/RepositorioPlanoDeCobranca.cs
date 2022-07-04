b16d1fb18b37b4aab23a0dd4f18219229811052d
Alan Fernandes <alanfernandes78@hotmail.com>
2022-07-04 17:03:58 -0300

    Implementado o RepositorioPlanoDeCobranca

---
+++ LocadoraFCVSJ.Infra.BancoDeDados/ModuloPlanoDeCobranca/RepositorioPlanoDeCobranca.cs
@@ -0,0 +1,100 @@
+﻿using LocadoraFCVSJ.Dominio.ModuloPlanoDeCobranca;
+using LocadoraFCVSJ.Infra.BancoDeDados.Compartilhado;
+using System.Data.SqlClient;
+
+namespace LocadoraFCVSJ.Infra.BancoDeDados.ModuloPlanoDeCobranca
+{
+    public class RepositorioPlanoDeCobranca : RepositorioBase<PlanoDeCobranca, MapeadorPlanoDeCobranca>, IRepositorioPlanoDeCobranca
+    {
+        protected override string QueryInserir =>
+            @"INSERT INTO [TBPlanoDeCobranca]
+                (
+		            [PLANODIARIO_VALORDIARIO],
+                    [PLANODIARIO_VALORKM],
+		            [PLANOLIVRE_VALORDIARIO],
+                    [PLANOCONTROLADO_VALORDIARIO],
+                    [PLANOCONTROLADO_VALORKM],
+                    [PLANOCONTROLADO_LIMITEKM],
+                    [GRUPO_ID]
+	            )
+                 VALUES
+                (
+		            @PLANODIARIO_VALORDIARIO,
+		            @PLANODIARIO_VALORKM,
+		            @PLANOLIVRE_VALORDIARIO,
+		            @PLANOCONTROLADO_VALORDIARIO,
+		            @PLANOCONTROLADO_VALORKM,
+		            @PLANOCONTROLADO_LIMITEKM,
+		            @GRUPO_ID
+	            )
+	            SELECT SCOPE_IDENTITY()";
+
+        protected override string QueryEditar =>
+            @"UPDATE [TBPlanoDeCobranca]
+	            SET
+		            [PLANODIARIO_VALORDIARIO] =	@PLANODIARIO_VALORDIARIO,
+                    [PLANODIARIO_VALORKM] = @PLANODIARIO_VALORKM,
+		            [PLANOLIVRE_VALORDIARIO] = @PLANOLIVRE_VALORDIARIO
+                    [PLANOCONTROLADO_VALORDIARIO] = @PLANOCONTROLADO_VALORDIARIO
+                    [PLANOCONTROLADO_VALORKM] = @PLANOCONTROLADO_VALORKM
+                    [PLANOCONTROLADO_LIMITEKM] = @PLANOCONTROLADO_LIMITEKM
+                    [GRUPO_ID] = @GRUPO_ID
+	            WHERE
+		            [ID] = @ID";
+
+        protected override string QueryExcluir => 
+            @"DELETE FROM [TBPlanoDeCobranca]
+                WHERE
+                    [ID] = @ID";
+
+        protected override string QuerySelecionarPorId =>
+			@"SELECT
+					PLANO.ID AS PLANO_ID,
+					PLANO.PLANODIARIO_VALORDIARIO AS PLANO_DIARIO_VALORDIARIO,
+					PLANO.PLANODIARIO_VALORKM AS PLANO_DIARIO_VALORKM,
+					PLANO.PLANOLIVRE_VALORDIARIO AS PLANO_LIVRE_VALORDIARIO,
+					PLANO.PLANOCONTROLADO_VALORDIARIO AS PLANO_CONTROLADO_VALORDIARIO,
+					PLANO.PLANOCONTROLADO_VALORKM AS PLANO_CONTROLADO_VALORKM,
+					PLANO.PLANOCONTROLADO_LIMITEKM AS PLANO_CONTROLADO_LIMITEKM,
+
+					GRUPO_ID AS GRUPO_ID,
+					GRUPO.Nome AS GRUPO_NOME
+				FROM 
+					[TBPlanoDeCobranca] AS PLANO
+
+					INNER JOIN
+
+					[TBGrupo] AS GRUPO
+
+					ON PLANO.GRUPO_ID = GRUPO.ID
+
+				WHERE
+					PLANO.ID = @ID";
+
+		protected override string QuerySelecionarTodos =>
+			@"SELECT
+					PLANO.ID AS PLANO_ID,
+					PLANO.PLANODIARIO_VALORDIARIO AS PLANO_DIARIO_VALORDIARIO,
+					PLANO.PLANODIARIO_VALORKM AS PLANO_DIARIO_VALORKM,
+					PLANO.PLANOLIVRE_VALORDIARIO AS PLANO_LIVRE_VALORDIARIO,
+					PLANO.PLANOCONTROLADO_VALORDIARIO AS PLANO_CONTROLADO_VALORDIARIO,
+					PLANO.PLANOCONTROLADO_VALORKM AS PLANO_CONTROLADO_VALORKM,
+					PLANO.PLANOCONTROLADO_LIMITEKM AS PLANO_CONTROLADO_LIMITEKM,
+
+					GRUPO_ID AS GRUPO_ID,
+					GRUPO.Nome AS GRUPO_NOME
+				FROM 
+					[TBPlanoDeCobranca] AS PLANO
+
+					INNER JOIN
+
+					[TBGrupo] AS GRUPO
+
+					ON PLANO.GRUPO_ID = GRUPO.ID";
+
+        public PlanoDeCobranca? SelecionarPropriedade<T>(string query, string parametro, T propriedade)
+        {
+			return SelecionarParametro(query, new SqlParameter(parametro, propriedade));
+		}
+	}
+}
