fba931d23c8d66139e05833ec889c64eeaaf0bda
Alan Fernandes <alanfernandes78@hotmail.com>
2022-07-05 14:19:07 -0300

    Implementado o ServicoPlanoDeCobranca

---
+++ LocadoraFCVSJ.Aplicacao/ModuloPlanoDeCobranca/ServicoPlanoDeCobranca.cs
@@ -0,0 +1,46 @@
+﻿using FluentValidation;
+using FluentValidation.Results;
+using LocadoraFCVSJ.Dominio.ModuloPlanoDeCobranca;
+using LocadoraFCVSJ.Infra.BancoDeDados.ModuloPlanoDeCobranca;
+
+namespace LocadoraFCVSJ.Aplicacao.ModuloPlanoDeCobranca
+{
+    public class ServicoPlanoDeCobranca
+    {
+        private readonly RepositorioPlanoDeCobranca _repositorioPlanoDeCobranca;
+
+        public ServicoPlanoDeCobranca(RepositorioPlanoDeCobranca repositorioPlanoDeCobranca)
+        {
+            _repositorioPlanoDeCobranca = repositorioPlanoDeCobranca;
+        }
+
+        public ValidationResult Inserir(PlanoDeCobranca planoDeCobranca)
+        {
+            ValidationResult resultadoValidacao = Validar(planoDeCobranca);
+
+            if (resultadoValidacao.IsValid)
+                _repositorioPlanoDeCobranca.Inserir(planoDeCobranca);
+
+            return resultadoValidacao;
+        }
+
+        public ValidationResult Editar(PlanoDeCobranca planoDeCobranca)
+        {
+            ValidationResult resultadoValidacao = Validar(planoDeCobranca);
+
+            if (resultadoValidacao.IsValid)
+                _repositorioPlanoDeCobranca.Editar(planoDeCobranca);
+
+            return resultadoValidacao;
+        }
+
+        private ValidationResult Validar(PlanoDeCobranca planoDeCobranca)
+        {
+            AbstractValidator<PlanoDeCobranca> validador = new ValidadorPlanoDeCobranca();
+
+            ValidationResult resultadoValidacao = validador.Validate(planoDeCobranca);
+
+            return resultadoValidacao;
+        }
+    }
+}
