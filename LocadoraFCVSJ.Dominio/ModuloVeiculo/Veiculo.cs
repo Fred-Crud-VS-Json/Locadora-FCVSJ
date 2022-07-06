f0e7e0232b5baf924366c1d27ce47e5f2310abd7
Alan Fernandes <alanfernandes78@hotmail.com>
2022-07-05 21:56:32 -0300

    Alterações no objeto Veiculo

--- LocadoraFCVSJ.Dominio/ModuloVeiculo/Veiculo.cs
+++ LocadoraFCVSJ.Dominio/ModuloVeiculo/Veiculo.cs
@@ -1,33 +1,10 @@
 ﻿using LocadoraFCVSJ.Dominio.Compartilhado;
 using LocadoraFCVSJ.Dominio.ModuloGrupo;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace LocadoraFCVSJ.Dominio.ModuloVeiculo
 {
-    public class Veiculo :EntidadeBase<Veiculo>
+    public class Veiculo : EntidadeBase<Veiculo>
     {
-        public Veiculo()
-        {
-        }
-
-        public Veiculo(Grupo grupoVeiculo, string modelo, string marca, string placa, string cor, TipoCombustivel? tipoCombustivel, decimal capacidadeTanque, int ano, decimal kmPercorrido, string detalhes)
-        {
-            GrupoVeiculo = grupoVeiculo;
-            Modelo = modelo;
-            Marca = marca;
-            Placa = placa;
-            Cor = cor;
-            TipoCombustivel = tipoCombustivel;
-            CapacidadeTanque = capacidadeTanque;
-            Ano = ano;
-            KmPercorrido = kmPercorrido;
-            Detalhes = detalhes;
-        }
-
         public Grupo GrupoVeiculo { get; set; }
         public string Modelo { get; set; }
         public string Marca { get; set; }
@@ -38,6 +15,7 @@ namespace LocadoraFCVSJ.Dominio.ModuloVeiculo
         public int Ano { get; set; }
         public decimal KmPercorrido { get; set; }
         public string Detalhes { get; set; }
+        public byte[] Foto { get; set; }
 
         public override bool Equals(object? obj)
         {
@@ -52,7 +30,8 @@ namespace LocadoraFCVSJ.Dominio.ModuloVeiculo
                    CapacidadeTanque == veiculo.CapacidadeTanque &&
                    Ano == veiculo.Ano &&
                    KmPercorrido == veiculo.KmPercorrido &&
-                   Detalhes == veiculo.Detalhes;
+                   Detalhes == veiculo.Detalhes &&
+                   EqualityComparer<byte[]>.Default.Equals(Foto, veiculo.Foto);
         }
 
         public override int GetHashCode()
@@ -69,6 +48,7 @@ namespace LocadoraFCVSJ.Dominio.ModuloVeiculo
             hash.Add(Ano);
             hash.Add(KmPercorrido);
             hash.Add(Detalhes);
+            hash.Add(Foto);
             return hash.ToHashCode();
         }
     }
