65963818ad2883fda67612c182385c397f9e5fd3
Alan Fernandes <alanfernandes78@hotmail.com>
2022-07-07 15:21:45 -0300

    Implementado validador separado para o plano diario

---
+++ LocadoraFCVSJ.Dominio/ModuloPlanoDeCobranca/ValidadorPlanoDiario.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+
+namespace LocadoraFCVSJ.Dominio.ModuloPlanoDeCobranca
+{
+    public class ValidadorPlanoDiario : AbstractValidator<PlanoDeCobranca>
+    {
+        public ValidadorPlanoDiario()
+        {
+            RuleFor(x => x.PlanoDiario_ValorDiario)
+                .GreaterThan(0);
+
+            RuleFor(x => x.PlanoDiario_ValorKm)
+                .GreaterThan(0);
+        }
+    }
+}
