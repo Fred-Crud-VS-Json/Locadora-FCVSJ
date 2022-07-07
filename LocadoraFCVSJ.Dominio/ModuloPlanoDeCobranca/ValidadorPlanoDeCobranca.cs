6f3878ee4b7ffd087a04546999b9ada58d74f92b
Alan Fernandes <alanfernandes78@hotmail.com>
2022-07-07 16:07:44 -0300

    Modificações no ValidadorPlanoDeCobranca

--- LocadoraFCVSJ.Dominio/ModuloPlanoDeCobranca/ValidadorPlanoDeCobranca.cs
+++ LocadoraFCVSJ.Dominio/ModuloPlanoDeCobranca/ValidadorPlanoDeCobranca.cs
@@ -6,23 +6,26 @@ namespace LocadoraFCVSJ.Dominio.ModuloPlanoDeCobranca
     {
         public ValidadorPlanoDeCobranca()
         {
+            RuleFor(x => x.Grupo)
+                .NotEmpty();
+
             RuleFor(x => x.PlanoDiario_ValorDiario)
-                .GreaterThan(0);
+                .NotEmpty();
 
             RuleFor(x => x.PlanoDiario_ValorKm)
-                .GreaterThan(0);
+                .NotEmpty();
 
             RuleFor(x => x.PlanoLivre_ValorDiario)
-                .GreaterThan(0);
+                .NotEmpty();
 
             RuleFor(x => x.PlanoControlado_ValorDiario)
-                .GreaterThan(0);
+                .NotEmpty();
 
             RuleFor(x => x.PlanoControlado_ValorKm)
-                 .GreaterThan(0);
+                .NotEmpty();
 
             RuleFor(x => x.PlanoControlado_LimiteKm)
-                .GreaterThan(0);
+                .NotEmpty();
         }
     }
 }
