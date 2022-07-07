1a2591b1179bdac05fa7398b349e279264d49a11
Alan Fernandes <alanfernandes78@hotmail.com>
2022-07-07 14:34:40 -0300

    Implementado o controlador com a tela de controle de plano de cobrança

---
+++ LocadoraFCVSJ/ModuloPlanoDeCobranca/ControladorPlanoDeCobranca.cs
@@ -0,0 +1,101 @@
+﻿using Krypton.Toolkit;
+using LocadoraFCVSJ.Aplicacao.ModuloPlanoDeCobranca;
+using LocadoraFCVSJ.Compartilhado;
+using LocadoraFCVSJ.Dominio.ModuloPlanoDeCobranca;
+using LocadoraFCVSJ.Infra.BancoDeDados.ModuloPlanoDeCobranca;
+
+namespace LocadoraFCVSJ.ModuloPlanoDeCobranca
+{
+    public class ControladorPlanoDeCobranca : ControladorBase
+    {
+        private readonly ServicoPlanoDeCobranca _servicoPlanoDeCobranca;
+        private readonly ControlePlanoDeCobrancaForm controlePlanoDeCobrancaForm;
+
+        public ControladorPlanoDeCobranca(ServicoPlanoDeCobranca servicoPlanoDeCobranca)
+        {
+            _servicoPlanoDeCobranca = servicoPlanoDeCobranca;
+            controlePlanoDeCobrancaForm = new(this);
+        }
+
+        public override void Inserir()
+        {
+            RegistrarNovoPlanoDeCobrancaForm tela = new()
+            {
+                PlanoDeCobranca = new(),
+                SalvarRegistro = _servicoPlanoDeCobranca.Inserir
+            };
+
+            DialogResult resultado = tela.ShowDialog();
+
+            if (resultado == DialogResult.OK)
+                CarregarPlanosDeCobranca();
+        }
+
+        public override void Editar()
+        {
+            PlanoDeCobranca? planoSelecionado = ObterPlanoDeCobranca();
+
+            if (planoSelecionado == null)
+            {
+                MessageBox.Show("Selecione um plano de cobrança primeiro.", "Edição de Plano de Cobrança", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            RegistrarNovoPlanoDeCobrancaForm tela = new()
+            {
+                PlanoDeCobranca = planoSelecionado,
+                SalvarRegistro = _servicoPlanoDeCobranca.Editar
+            };
+
+            tela.label1.Text = "                Editando Registro";
+
+            DialogResult resultado = tela.ShowDialog();
+
+            if (resultado == DialogResult.OK)
+                CarregarPlanosDeCobranca();
+        }
+
+        public override void Excluir()
+        {
+            PlanoDeCobranca? planoSelecionado = ObterPlanoDeCobranca();
+
+            if (planoSelecionado == null)
+            {
+                MessageBox.Show("Selecione um plano de cobrança primeiro.", "Exclusão de Plano de Cobrança", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            DialogResult resultado = MessageBox.Show("Deseja realmente excluir este registro?", "Exclusão de Plano de Cobrança", MessageBoxButtons.OKCancel, MessageBoxIcon.Question);
+
+            if (resultado == DialogResult.OK)
+                _servicoPlanoDeCobranca.Excluir(planoSelecionado);
+
+            CarregarPlanosDeCobranca();
+        }
+
+        public override KryptonForm ObterTela()
+        {
+            CarregarPlanosDeCobranca();
+
+            return controlePlanoDeCobrancaForm;
+        }
+
+        private void CarregarPlanosDeCobranca()
+        {
+            List<PlanoDeCobranca> planosDeCobranca = _servicoPlanoDeCobranca.SelecionarTodos();
+
+            controlePlanoDeCobrancaForm.AtualizarGrid(planosDeCobranca);
+        }
+
+        private PlanoDeCobranca? ObterPlanoDeCobranca()
+        {
+            if (controlePlanoDeCobrancaForm.ObterGrid().CurrentCell != null && controlePlanoDeCobrancaForm.ObterGrid().CurrentCell.Selected == true)
+            {
+                int index = controlePlanoDeCobrancaForm.ObterLinhaSelecionada();
+                return _servicoPlanoDeCobranca.SelecionarTodos().ElementAtOrDefault(index);
+            }
+
+            return null;
+        }
+    }
+}
---
+++ LocadoraFCVSJ/ModuloPlanoDeCobranca/ControlePlanoDeCobrancaForm.cs
@@ -0,0 +1,92 @@
+﻿using Krypton.Toolkit;
+using LocadoraFCVSJ.Dominio.ModuloPlanoDeCobranca;
+
+namespace LocadoraFCVSJ.ModuloPlanoDeCobranca
+{
+    public partial class ControlePlanoDeCobrancaForm : KryptonForm
+    {
+        private readonly ControladorPlanoDeCobranca _controladorPlanoDeCobranca;
+
+        public ControlePlanoDeCobrancaForm(ControladorPlanoDeCobranca controladorPlanoDeCobranca)
+        {
+            InitializeComponent();
+            _controladorPlanoDeCobranca = controladorPlanoDeCobranca;
+        }
+
+        public void AtualizarGrid(List<PlanoDeCobranca> planosDeCobrancas)
+        {
+            GridPlanos.Rows.Clear();
+
+            planosDeCobrancas.ForEach(x =>
+            {
+                GridPlanos.Rows.Add(x.Id, x.Grupo.Nome);
+            });
+
+            GridPlanos.ClearSelection();
+        }
+
+        public int ObterLinhaSelecionada()
+        {
+            return GridPlanos.CurrentCell.RowIndex;
+        }
+
+        public DataGridView ObterGrid()
+        {
+            return GridPlanos;
+        }
+
+        private void ControlePlanoDeCobrancaForm_Load(object sender, EventArgs e)
+        {
+            GridPlanos.ClearSelection();
+        }
+
+        private void BtnInserir_Click(object sender, EventArgs e)
+        {
+            _controladorPlanoDeCobranca.Inserir();
+        }
+
+        private void BtnEditar_Click(object sender, EventArgs e)
+        {
+            _controladorPlanoDeCobranca.Editar();
+        }
+
+        private void BtnExcluir_Click(object sender, EventArgs e)
+        {
+            _controladorPlanoDeCobranca.Excluir();
+        }
+
+        private void GridPlanos_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void GridPlanos_CellPainting(object sender, DataGridViewCellPaintingEventArgs e)
+        {
+            Image someImage = Properties.Resources.search_more_25px;
+
+            if (e.RowIndex < 0)
+                return;
+
+            if (e.ColumnIndex == 2)
+            {
+                e.Paint(e.CellBounds, DataGridViewPaintParts.All);
+
+                var w = someImage.Width;
+                var h = someImage.Height;
+                var x = e.CellBounds.Left + (e.CellBounds.Width - w) / 2;
+                var y = e.CellBounds.Top + (e.CellBounds.Height - h) / 2;
+
+                e.Graphics.DrawImage(someImage, new Rectangle(x, y, w, h));
+                e.Handled = true;
+            }
+        }
+
+        private void GridPlanos_CellFormatting(object sender, DataGridViewCellFormattingEventArgs e)
+        {
+            DataGridViewCell cell = GridPlanos.Rows[e.RowIndex].Cells[e.ColumnIndex];
+
+            if (e.ColumnIndex == 8)
+                cell.ToolTipText = "Visualizar Informações do Plano";
+        }
+    }
+}
