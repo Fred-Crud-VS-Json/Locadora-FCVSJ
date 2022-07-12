f3c2bce4b90aa2dfadbc224a7909ab52299e963a
Alan Fernandes <alanfernandes78@hotmail.com>
2022-07-12 14:23:58 -0300

    Implementado logs no ServicoPlanoDeCobranca

--- LocadoraFCVSJ.Aplicacao/ModuloPlanoDeCobranca/ServicoPlanoDeCobranca.cs
+++ LocadoraFCVSJ.Aplicacao/ModuloPlanoDeCobranca/ServicoPlanoDeCobranca.cs
@@ -2,6 +2,7 @@
 using FluentValidation.Results;
 using LocadoraFCVSJ.Dominio.ModuloPlanoDeCobranca;
 using LocadoraFCVSJ.Infra.BancoDeDados.ModuloPlanoDeCobranca;
+using Serilog;
 
 namespace LocadoraFCVSJ.Aplicacao.ModuloPlanoDeCobranca
 {
@@ -16,20 +17,40 @@ namespace LocadoraFCVSJ.Aplicacao.ModuloPlanoDeCobranca
 
         public ValidationResult Inserir(PlanoDeCobranca planoDeCobranca)
         {
+            Log.Logger.Debug("Tentando inserir novo plano de cobrança...");
+
             ValidationResult resultadoValidacao = Validar(planoDeCobranca);
 
             if (resultadoValidacao.IsValid)
+            {
                 _repositorioPlanoDeCobranca.Inserir(planoDeCobranca);
+                Log.Logger.Information($"Plano de Cobrança {planoDeCobranca.Id} inserido com sucesso!");
+            }
+            else
+            {
+                foreach (ValidationFailure? erro in resultadoValidacao.Errors)
+                    Log.Logger.Warning($"Falha ao tentar inserir o plano de cobrança {planoDeCobranca.Id} - {erro.ErrorMessage}");
+            }
 
             return resultadoValidacao;
         }
 
         public ValidationResult Editar(PlanoDeCobranca planoDeCobranca)
         {
+            Log.Logger.Debug("Tentando editar plano de cobrança...");
+
             ValidationResult resultadoValidacao = Validar(planoDeCobranca);
 
             if (resultadoValidacao.IsValid)
+            {
                 _repositorioPlanoDeCobranca.Editar(planoDeCobranca);
+                Log.Logger.Information($"Plano de Cobrança {planoDeCobranca.Id} editado com sucesso!");
+            }
+            else
+            {
+                foreach (ValidationFailure? erro in resultadoValidacao.Errors)
+                    Log.Logger.Warning($"Falha ao tentar editar o plano de cobrança {planoDeCobranca.Id} - {erro.ErrorMessage}");
+            }
 
             return resultadoValidacao;
         }
