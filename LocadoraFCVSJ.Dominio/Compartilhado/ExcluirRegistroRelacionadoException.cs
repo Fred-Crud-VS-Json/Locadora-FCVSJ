9883061393a0e8770eddee6875631fd67532d983
Alan Fernandes <alanfernandes78@hotmail.com>
2022-07-14 16:12:47 -0300

    Criado a exceção personalizada ExcluirRegistroRelacionadoException

---
+++ LocadoraFCVSJ.Dominio/Compartilhado/ExcluirRegistroRelacionadoException.cs
@@ -0,0 +1,9 @@
+﻿using System.Data.SqlClient;
+
+namespace LocadoraFCVSJ.Dominio.Compartilhado
+{
+    public class ExcluirRegistroRelacionadoException : Exception
+    {
+        public ExcluirRegistroRelacionadoException(SqlException ex) : base("", ex) { }
+    }
+}
