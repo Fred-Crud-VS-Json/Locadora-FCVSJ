257a1e28246b43ee78a806a171670c9d3781cfeb
Alan Fernandes <alanfernandes78@hotmail.com>
2022-07-18 05:25:57 -0300

    Adicionado componentes de UI customizados

---
+++ LocadoraFCVSJ/Compartilhado/Componentes/CustomKryptonButton.cs
@@ -0,0 +1,19 @@
+﻿using Krypton.Toolkit;
+
+namespace LocadoraFCVSJ.Compartilhado.Componentes
+{
+    public class CustomKryptonButton : KryptonButton
+    {
+        protected override void OnMouseLeave(EventArgs e)
+        {
+            base.OnMouseLeave(e);
+            Cursor.Current = Cursors.Default;
+        }
+
+        protected override void OnMouseMove(MouseEventArgs e)
+        {
+            base.OnMouseMove(e);
+            Cursor.Current = Cursors.Hand;
+        }
+    }
+}
