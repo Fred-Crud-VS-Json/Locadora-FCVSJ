4b2bb27ef8a885f27491450a0b43719a9f95d967
Alan Fernandes <alanfernandes78@hotmail.com>
2022-07-18 05:25:44 -0300

    Adicionado arquivo de extensões do DataGridView

---
+++ LocadoraFCVSJ/Compartilhado/DataGridViewExtensions.cs
@@ -0,0 +1,18 @@
+﻿namespace LocadoraFCVSJ.Compartilhado
+{
+    public static class DataGridViewExtensions
+    {
+        public static void ConfigurarImagem(this DataGridViewCellPaintingEventArgs e, Image image)
+        {
+            e.Paint(e.CellBounds, DataGridViewPaintParts.All);
+
+            var w = image.Width;
+            var h = image.Height;
+            var x = e.CellBounds.Left + (e.CellBounds.Width - w) / 2;
+            var y = e.CellBounds.Top + (e.CellBounds.Height - h) / 2;
+
+            e.Graphics.DrawImage(image, new Rectangle(x, y, w, h));
+            e.Handled = true;
+        }
+    }
+}
