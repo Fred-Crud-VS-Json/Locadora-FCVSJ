1d2072e72cf2519d612afb1c30d3073eef904d88
Alan Fernandes <alanfernandes78@hotmail.com>
2022-07-18 05:23:08 -0300

    Adicionado arquivo StringExtensions.cs

---
+++ LocadoraFCVSJ.Dominio/Compartilhado/StringExtensions.cs
@@ -0,0 +1,27 @@
+﻿namespace LocadoraFCVSJ.Dominio.Compartilhado
+{
+    public static class StringExtensions
+    {
+        public static string FormatarParaCpf(this string cpf)
+        {
+            string response = cpf.Trim();
+            if (response.Length == 11)
+            {
+                response = response.Insert(9, "-");
+                response = response.Insert(6, ".");
+                response = response.Insert(3, ".");
+            }
+            return response;
+        }
+
+        public static string FormatarParaTelefone(this string telefone)
+        {
+            string p1 = telefone[..2];
+            string p2 = telefone.Substring(2, 1);
+            string p3 = telefone.Substring(3, 4);
+            string p4 = telefone.Substring(7, 4);
+
+            return $"({p1}) {p2} {p3}-{p4}";
+        }
+    }
+}
