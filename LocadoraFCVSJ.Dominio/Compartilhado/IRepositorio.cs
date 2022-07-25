16bf6854c508783cdeb170ed517b49f6351e8d19
Alan Fernandes <alanfernandes78@hotmail.com>
2022-07-25 16:36:09 -0300

    Alterado local da classe IRepositorio

---
+++ LocadoraFCVSJ.Dominio/Compartilhado/IRepositorio.cs
@@ -0,0 +1,11 @@
+﻿namespace LocadoraFCVSJ.Dominio.Compartilhado
+{
+    public interface IRepositorio<T> where T : EntidadeBase<T>
+    {
+        void Inserir(T registro);
+        void Editar(T registro);
+        void Excluir(T registro);
+        List<T> SelecionarTodos();
+        T? SelecionarPorId(Guid id);
+    }
+}
