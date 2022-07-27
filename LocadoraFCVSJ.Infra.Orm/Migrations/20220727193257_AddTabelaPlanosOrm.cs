8dc06d1c80d2315a38187fde48588e2eafda95af
Alan Fernandes <alanfernandes78@hotmail.com>
2022-07-27 18:16:06 -0300

    Implementado o módulo Plano de Cobrança com EntityFramework Core

---
+++ LocadoraFCVSJ.Infra.Orm/Migrations/20220727193257_AddTabelaPlanosOrm.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LocadoraFCVSJ.Infra.Orm.Migrations
+{
+    public partial class AddTabelaPlanosOrm : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TBPlanoOrm",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    GrupoId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    PlanoDiario_ValorDiario = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    PlanoDiario_ValorKm = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    PlanoLivre_ValorDiario = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    PlanoControlado_ValorDiario = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    PlanoControlado_ValorKm = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    PlanoControlado_LimiteKm = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TBPlanoOrm", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_TBPlanoOrm_TBGrupoOrm_GrupoId",
+                        column: x => x.GrupoId,
+                        principalTable: "TBGrupoOrm",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TBPlanoOrm_GrupoId",
+                table: "TBPlanoOrm",
+                column: "GrupoId",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TBPlanoOrm");
+        }
+    }
+}
---
+++ LocadoraFCVSJ.Infra.Orm/ModuloPlanoDeCobranca/RepositorioPlanoDeCobrancaOrm.cs
@@ -0,0 +1,17 @@
+﻿using LocadoraFCVSJ.Dominio.Compartilhado.Interfaces;
+using LocadoraFCVSJ.Dominio.ModuloPlanoDeCobranca;
+using LocadoraFCVSJ.Infra.Orm.Compartilhado;
+using Microsoft.EntityFrameworkCore;
+
+namespace LocadoraFCVSJ.Infra.Orm.ModuloPlanoDeCobranca
+{
+    public class RepositorioPlanoDeCobrancaOrm : RepositorioBaseOrm<PlanoDeCobranca>, IRepositorioPlanoDeCobranca
+    {
+        public DbSet<PlanoDeCobranca> Planos { get; set; }
+
+        public RepositorioPlanoDeCobrancaOrm(IContextoPersistencia dbContext) : base(dbContext)
+        {
+            Planos = _dbSet;
+        }
+    }
+}
