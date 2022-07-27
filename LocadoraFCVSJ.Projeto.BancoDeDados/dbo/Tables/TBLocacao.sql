fdf85b9cada223e57495a5a52bf363db3f1332f2
Pedro Nunes <pedronunesjc@gmail.com>
2022-07-27 16:55:24 -0300

    Criada a TBLocacao

---
+++ LocadoraFCVSJ.Projeto.BancoDeDados/dbo/Tables/TBLocacao.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[TBLocacao] (
+    [Id]                 UNIQUEIDENTIFIER NOT NULL,
+    [Cliente_ID]         UNIQUEIDENTIFIER NOT NULL,
+    [DataLocacao]        DATETIME         NOT NULL,
+    [DataDevolucao]      DATETIME         NOT NULL,
+    [Veiculo_ID]         UNIQUEIDENTIFIER NOT NULL,
+    [Taxa_ID]            UNIQUEIDENTIFIER NOT NULL,
+    [precoEstimado]      DECIMAL (18)     NOT NULL,
+    [PlanoDeCobranca_ID] UNIQUEIDENTIFIER NOT NULL,
+    CONSTRAINT [PK_TBLocacao] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
