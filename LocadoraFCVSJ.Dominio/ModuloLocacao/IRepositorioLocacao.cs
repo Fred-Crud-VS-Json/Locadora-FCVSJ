a82d01598b7ce5d4ccc558b27a75f461a8290ae9
Pedro Nunes <pedronunesjc@gmail.com>
2022-07-27 17:29:31 -0300

    Molde do moduloLocacao

---
+++ LocadoraFCVSJ.Dominio/ModuloLocacao/IRepositorioLocacao.cs
@@ -0,0 +1,10 @@
+﻿using LocadoraFCVSJ.Dominio.Compartilhado;
+
+namespace LocadoraFCVSJ.Dominio.ModuloLocacao
+{
+    //public class IRepositorioLocacao : IRepositorio<Locacao>
+    //{
+    //    Locacao? SelecionarPorUsuario(string usuario);
+
+    //}
+}
